130ec17b3072ce107994610159b3b4363b90f314
traore362 <mtpmtp362@gmail.com>
2025-01-10 19:27:55 +0000

    ma première commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,11 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      
+      <Text style={styles.title}>Tab Moustapha traore</Text>
+      <Text style={styles.title}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente nam impedit sed animi deleniti quo minus blanditiis eveniet perspiciatis! Distinctio quisquam quo harum repellendus obcaecati tempore aliquid vero quidem sit.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quos facere aliquid labore autem cupiditate obcaecati ea voluptas iste! Earum quis dolore magni nam placeat porro minus eligendi modi praesentium.
+      </Text>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
